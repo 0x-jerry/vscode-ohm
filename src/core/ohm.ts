e080c3e07e5e3bad737596be9ef19968c87d2553
Jerry <x.jerry.wang@gmail.com>
2026-01-30 22:34:53 +0800

    perf: add more builtin rules

--- src/core/ohm.ts
+++ src/core/ohm.ts
@@ -30,8 +30,9 @@ export interface BuiltinRule {
 
 /**
  * https://ohmjs.org/docs/syntax-reference#built-in-rules
- *
+ * 
  * https://github.com/ohmjs/ohm/blob/cb3decc0b8d8c6300b7f591f13d5219314a0e3c0/packages/ohm-js/src/built-in-rules.ohm
+ * https://github.com/ohmjs/ohmjs.org/blob/1a1ae6160dca2a87300c45c103721abe24048856/docs/syntax-reference.md#built-in-rules
  *
  */
 export const builtinRules: BuiltinRule[] = [
@@ -68,20 +69,32 @@ export const builtinRules: BuiltinRule[] = [
       'Matches a single whitespace character (e.g., space, tab, newline, etc.)',
   },
   {
+    label: 'spaces',
+    documentation:
+      'Matches multiple whitespace characters (e.g., space, tab, newline, etc.)',
+  },
+  {
     label: 'end',
     documentation: 'Matches the end of the input stream. Equivalent to ~any.',
   },
   {
-    label: 'ListOf',
+    label: 'caseInsensitive',
+    documentation: "Matches _terminal_, but ignoring any differences in casing (based on the simple, single-character Unicode case mappings)"
   },
   {
-    label: 'EmptyListOf',
+    label: 'ListOf',
+    documentation: "Matches the expression _elem_ zero or more times, separated by something that matches the expression _sep_."
   },
   {
     label: 'NonemptyListOf',
+    documentation: "Like `ListOf`, but matches _elem_ at least one time."
   },
   {
     label: 'listOf',
+    documentation: "Similar to `ListOf<elem, sep>` but interpreted as lexical rule."
+  },
+  {
+    label: 'EmptyListOf',
   },
   {
     label: 'emptyListOf',
@@ -91,9 +104,20 @@ export const builtinRules: BuiltinRule[] = [
   },
   {
     label: 'applySyntactic',
+    documentation: " Allows the syntactic rule _ruleName_ to be applied in a lexical context, which is otherwise not allowed. Spaces are skipped _before_ and _after_ the rule application. _New in Ohm v16.1.0._"
   },
   {
-    label: 'caseInsensitive',
+    label: "unicodeChar",
+    documentation: "matches a single Unicode code point from a given category, or with a given binary property."
+  },
+  {
+    label: 'any',
+    documentation: "Matches the next Unicode character — i.e., a single code point — in the input stream, if one exists."
+  },
+  {
+    label: 'unicodeLtmo',
+    // https://github.com/ohmjs/ohm/blob/1895b4e42da2528872b406288d44f8e354411944/packages/ohm-js/src/Grammar.js#L352-L358
+    documentation: "Union of Lt (titlecase), Lm (modifier), and Lo (other), i.e. any letter not in Ll or Lu."
   },
 ]
 
